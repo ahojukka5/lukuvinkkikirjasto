274d181961de453ff89aaee8965a60be1a527fb0
chzesa <chzesa@gmail.com>
2021-04-03 17:29:16 +0300

    Set main program to instantiate jsonDao

--- app/src/main/java/lukuvinkkikirjasto/App.java
+++ app/src/main/java/lukuvinkkikirjasto/App.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 import lukuvinkkikirjasto.domain.TipService;
 import lukuvinkkikirjasto.ui.ConsoleUi;
-import lukuvinkkikirjasto.dao.InMemoryTipDao;
+import lukuvinkkikirjasto.dao.JsonDao;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        TipService tipService = new TipService(new InMemoryTipDao());
+        TipService tipService = new TipService(new JsonDao("file.txt"));
         new ConsoleUi(scanner, tipService).start();
     }
 }
