5e866de30457d73baed4add182d26d6eaa1f48eb
Jukka Aho <ahojukka5@gmail.com>
2021-04-14 17:12:51 +0700

    Add remove and findById to TipDao interface

--- app/src/main/java/lukuvinkkikirjasto/dao/TipDao.java
+++ app/src/main/java/lukuvinkkikirjasto/dao/TipDao.java
@@ -1,8 +1,14 @@
 package lukuvinkkikirjasto.dao;
+
 import java.util.List;
 import lukuvinkkikirjasto.domain.Tip;
 
 public interface TipDao {
     void create(Tip tip);
+
+    void remove(Tip tip);
+
+    Tip findById(Integer id);
+
     List<Tip> getAll();
 }
