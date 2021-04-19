393fd8a6b590f92682b42021c97fd8b0dd0a1496
jkotima <jkotima@gmail.com>
2021-04-19 10:53:09 +0300

    Unit tests for tipService, InMemoryTipDao-class to public

---
+++ app/src/test/java/lukuvinkkikirjasto/domain/TipServiceTest.java
@@ -0,0 +1,55 @@
+package lukuvinkkikirjasto.domain;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import lukuvinkkikirjasto.dao.InMemoryTipDao;
+import lukuvinkkikirjasto.domain.Tip;
+import lukuvinkkikirjasto.domain.matcher.*;
+import java.util.*;
+public class TipServiceTest {
+    private TipService tipService;
+    private InMemoryTipDao dao;
+
+    @Before
+    public void initialize() {
+        dao = new InMemoryTipDao();
+        tipService = new TipService(dao);
+    }
+
+    @Test
+    public void createTipWorks() {
+        tipService.createTip("aa", "example.com");
+        assertTrue(tipService.getAll().get(0).getTitle().equals("aa"));
+        assertTrue(tipService.getAll().get(0).getUrl().equals("example.com"));
+    }
+
+    @Test
+    public void matchesAllWorksWithTitleContains() {
+        Tip tip1 = new Tip(1, "yykaakoo", "example1.com");
+        Tip tip2 = new Tip(1, "kookaayy", "example2.com");
+        Tip tip3 = new Tip(1, "jeejeejee", "example3.com");
+        dao.create(tip1);
+        dao.create(tip2);
+        dao.create(tip3);
+
+        List<Matcher> matcherList = new ArrayList<Matcher>();
+
+        assertTrue(tipService.matchesAll(matcherList).contains(tip1));
+        assertTrue(tipService.matchesAll(matcherList).contains(tip2));
+        assertTrue(tipService.matchesAll(matcherList).contains(tip3));
+
+        matcherList.add(new TitleContains("yy"));
+        assertTrue(tipService.matchesAll(matcherList).contains(tip1));
+        assertTrue(tipService.matchesAll(matcherList).contains(tip2));
+        assertFalse(tipService.matchesAll(matcherList).contains(tip3));
+
+        matcherList.add(new TitleContains("kaakoo"));
+        assertTrue(tipService.matchesAll(matcherList).contains(tip1));
+        assertFalse(tipService.matchesAll(matcherList).contains(tip2));
+        assertFalse(tipService.matchesAll(matcherList).contains(tip3));
+
+        matcherList.add(new TitleContains("jepulis"));
+        assertTrue(tipService.matchesAll(matcherList).size() == 0);
+    }
+    
+}
