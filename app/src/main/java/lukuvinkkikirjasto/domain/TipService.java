0448767b800bff4f3305e4acd96d0039f8684dac
Jukka Aho <ahojukka5@gmail.com>
2021-04-19 18:27:17 +0700

    TipService.java: fix checkstyle errors

--- app/src/main/java/lukuvinkkikirjasto/domain/TipService.java
+++ app/src/main/java/lukuvinkkikirjasto/domain/TipService.java
@@ -1,15 +1,16 @@
-// Sovelluslogiikasta vastaava luokka
 package lukuvinkkikirjasto.domain;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import lukuvinkkikirjasto.dao.TipDao;
-import lukuvinkkikirjasto.domain.matcher.Matcher;
-import lukuvinkkikirjasto.domain.matcher.And;
-import lukuvinkkikirjasto.domain.matcher.All;
-
 import lukuvinkkikirjasto.domain.Tip;
+import lukuvinkkikirjasto.domain.matcher.All;
+import lukuvinkkikirjasto.domain.matcher.And;
+import lukuvinkkikirjasto.domain.matcher.Matcher;
 
+/**
+ * TipService class.
+ */
 public class TipService {
     private TipDao tipDao;
 
@@ -17,6 +18,12 @@ public class TipService {
         this.tipDao = tipDao;
     }
 
+    /**
+     * Create new tip.
+     *
+     * @param title title of tip
+     * @param url url of tip
+     */
     public void createTip(String title, String url) {
         int id = tipDao.getAll().size() + 1;
         Tip tip = new Tip(id, title, url);
@@ -27,6 +34,12 @@ public class TipService {
         return tipDao.getAll();
     }
 
+    /**
+     * Return all matches of tip.
+     *
+     * @param matcher is iterable of matchers
+     * @return matches
+     */
     public List<Tip> matches(Matcher matcher) {
         ArrayList<Tip> matches = new ArrayList<Tip>();
         
@@ -38,6 +51,12 @@ public class TipService {
         return matches;
     }
 
+    /**
+     * Check does tip match to all matchers.
+     *
+     * @param matchers is iterable of matchers
+     * @return boolean value
+     */
     public List<Tip> matchesAll(List<Matcher> matchers) {
         Matcher finalMatcher = new All();
 
