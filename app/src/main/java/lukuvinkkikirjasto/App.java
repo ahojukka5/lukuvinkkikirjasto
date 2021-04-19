d9f3ff9a06ed15f36eea3536e837468d6c5edeb0
Jukka Aho <ahojukka5@gmail.com>
2021-04-19 18:01:14 +0700

    App.java: fix checkstyle errors

--- app/src/main/java/lukuvinkkikirjasto/App.java
+++ app/src/main/java/lukuvinkkikirjasto/App.java
@@ -1,12 +1,18 @@
 package lukuvinkkikirjasto;
 
 import java.util.Scanner;
-
+import lukuvinkkikirjasto.dao.JsonDao;
 import lukuvinkkikirjasto.domain.TipService;
 import lukuvinkkikirjasto.ui.ConsoleUi;
-import lukuvinkkikirjasto.dao.JsonDao;
 
+/**
+ * Main App class.
+ */
 public class App {
+
+    /**
+     * Start app with console UI.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TipService tipService = new TipService(new JsonDao("file.txt"));
